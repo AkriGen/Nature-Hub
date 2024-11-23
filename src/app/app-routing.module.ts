1637eb5a8f25b6c7cf60d8d8b3812cbf71086485
AkriGen <akriti.singh@genpact.com>
2024-11-23 23:32:49 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RemediesComponent } from './remedies/remedies.component';
+import { HealthtipsComponent } from './healthtips/healthtips.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'remedies',component:RemediesComponent},
+  {path:'healthtips',component:HealthtipsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RemediesComponent } from './remedies/remedies.component';
+import { HealthtipsComponent } from './healthtips/healthtips.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RemediesComponent,
+    HealthtipsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
